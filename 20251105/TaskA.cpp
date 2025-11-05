e1e39cc3509d9c245bc8133f898feb8ed138055c
kazu06121 <ymkazu1@gmail.com>
2025-11-05 15:27:19 +0900

    ignoreの追加

---
+++ 20251105/TaskA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    const int SIZE = 10;
+    int num[SIZE];
+    
+    std::srand(std::time(nullptr));
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        num[i] = std::rand() % 50;
+    }
+}
