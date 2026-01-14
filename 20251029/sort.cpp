ef7c2d476248d12d0cc52f03919191070e4b5dc5
kazu06121 <ymkazu1@gmail.com>
2026-01-14 16:06:00 +0900

    修正

--- 20251029/sort.cpp
+++ 20251029/sort.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 
 int main()
 {
     const int SIZE = 10;
     int num[SIZE];
 
-    std::srand(std::time(nullptr));
+    std::srand((unsigned int)std::time(nullptr));
 
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE; i++)      //乱数代入
     {
-        num[i] = std::rand() % 50;      //50までの乱数を代入
+        num[i] = std::rand() % 50;
     }
 
-    std::sort(num, num + SIZE);         //ソート
+    std::cout << "ソート前";
+    for (int i = 0; i < SIZE; i++)      //ソート前の表示
+    {
+        std::cout <<num[i] << " ";
+    }
 
-    for (int i = 0; i < SIZE; i++)      //配列の出力
+    std::cout << std::endl;
+    
+    for (int i = 0; i < SIZE - 1; i++)  //ソート
+    {
+        for (int j = 0; j < SIZE - 1 - i; j++)
+        {
+            if (num[j] > num[j + 1])
+            {
+                int tmp = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = tmp;
+            }
+        }
+    }
+
+    std::cout <<"ソート後";
+    for (int i = 0; i < SIZE; i++)
     {
         std::cout << num[i] << " ";
     }
     std::cout << std::endl;
-    return 0;                           //終了
+
+    return 0;
 }
